192dc3b962084396c4d2cb1fb319fdc3619b66b2
Bourn3x <julianchong10@gmail.com>
2020-04-22 18:28:04 +0800

    Added Welcome Back container in middle column

--- dashboard.js
+++ dashboard.js
@@ -1,3 +1,31 @@
+let classes = {
+    FIT1008: {
+        title: "Introduction to Computer Science",
+        progress: "70",
+    },
+    FIT2004: {
+        title: "Algorithms and Data Structures",
+        progress: "30",
+    }
+}
+
+let user = {
+    id: 1,
+    name: "Punk",
+    lastClass: "FIT1008",
+}
+
+function fillWelcomeBackContainer(user, classes) {
+    $("#welcomeBack").html("Welcome back, " + user.name + "!")
+    lastClass = classes[user.lastClass]
+    $("#lastClassUnit").html(user.lastClass + " - " + lastClass.title)
+    $("#lastClassProgressBar").css("width", lastClass.progress + "%")
+}
+
+function fillProgressBars() {
+
+}
+
 $( document ).ready(function() {
     if ($(window).width() >= 992) {
         $('#collapseContainer').removeClass('collapse');
@@ -11,4 +39,6 @@ $( document ).ready(function() {
             $('#collapseContainer').addClass('collapse');
         }
     });
+
+    fillWelcomeBackContainer(user, classes);
 });
