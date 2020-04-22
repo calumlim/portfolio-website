4e0ab8144c53872754376f095fac8a9491011b73
Bourn3x <julianchong10@gmail.com>
2020-04-23 00:05:41 +0800

    Added classes list

--- dashboard.js
+++ dashboard.js
@@ -1,18 +1,48 @@
-let classes = {
+const classes = {
     FIT1008: {
         title: "Introduction to Computer Science",
-        progress: "70",
+        progress: "50",
     },
     FIT2004: {
         title: "Algorithms and Data Structures",
         progress: "30",
+    },
+    FIT2014: {
+        title: "Theory Of Computation",
+        progress: "15",
+    },
+    FIT3155: {
+        title: "AADS",
+        progress: "70",
+    },
+    FIT3171: {
+        title: "Databases",
+        progress: "100",
     }
 }
 
-let user = {
+const user = {
     id: 1,
     name: "Punk",
-    lastClass: "FIT1008",
+    lastClass: "FIT2014",
+}
+
+function createClassRow(unitName) {
+    title = classes[unitName].title
+    progress = classes[unitName].progress + "%"
+    return (
+        `<div class="class-info">
+            <div class="unit-name">
+                ${unitName} - ${title}
+            </div>
+            <div class="unit-progress-bar">
+                <div class="progress-bar-container">
+                    <div class="progress-bar-fill-2" style="width:${progress}"></div>
+                </div>
+            </div>
+            <p class="unit-progress-percentage">${progress}</p>
+        </div>`
+    )
 }
 
 function fillWelcomeBackContainer(user, classes) {
@@ -20,25 +50,30 @@ function fillWelcomeBackContainer(user, classes) {
     lastClass = classes[user.lastClass]
     $("#lastClassUnit").html(user.lastClass + " - " + lastClass.title)
     $("#lastClassProgressBar").css("width", lastClass.progress + "%")
+    $("#progressPercentage").html(lastClass.progress + "%")
 }
 
-function fillProgressBars() {
-
+function fillClassesList(classes) {
+    const keys = Object.keys(classes)
+    keys.forEach((unitName) => {
+        $("#classList").append(createClassRow(unitName))
+    })
 }
 
 $( document ).ready(function() {
     if ($(window).width() >= 992) {
-        $('#collapseContainer').removeClass('collapse');
+        $('#collapseContainer').removeClass('collapse')
     }
 
-    $(window).resize(function(){
+    $(window).resize(function() {
         if ($(window).width() >= 992) {
-            $('#collapseContainer').removeClass('collapse');
+            $('#collapseContainer').removeClass('collapse')
         }
         else {
-            $('#collapseContainer').addClass('collapse');
+            $('#collapseContainer').addClass('collapse')
         }
     });
 
-    fillWelcomeBackContainer(user, classes);
-});
+    fillWelcomeBackContainer(user, classes)
+    fillClassesList(classes)
+})
