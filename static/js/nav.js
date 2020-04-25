2d965006371401b4815370f630bec5900c33654e
Calum <calumstah@gmail.com>
2020-04-26 02:44:46 +0800

    Added animate.css library, fadein fadeout animation for responsive menu

--- static/js/nav.js
+++ static/js/nav.js
@@ -1,9 +1,14 @@
 $(document).ready(function() {
     $('#open').on('click', function() {
         $('#overlay').removeClass('close');
+        $('#overlay').removeClass('animated fadeOut');
+        $('#overlay').addClass('animated fadeIn');
     });
 
     $('#close').on('click', function() {
-        $('#overlay').addClass('close');
+        $('#overlay').addClass('animated fadeOut');
+        setTimeout(function() {
+            $('#overlay').addClass('close');
+        }, 1000);
     });
 });
