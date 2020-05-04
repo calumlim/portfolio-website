e7d6ca0206c6be01fafa86c7ad96a5e0daf195be
Calum <calumstah@gmail.com>
2020-05-04 18:04:22 +0800

    feat/ Added smooth scrolling for navigation links

--- static/js/nav.js
+++ static/js/nav.js
@@ -3,12 +3,32 @@ $(document).ready(function() {
         $('#overlay').removeClass('close');
         $('#overlay').removeClass('animated fadeOut');
         $('#overlay').addClass('animated fadeIn');
+        $("section").css("display", "none");
     });
 
     $('#close').on('click', function() {
         $('#overlay').addClass('animated fadeOut');
+        $("section").css("display", "block");
+        $("#Footer").css("display", "grid");
         setTimeout(function() {
             $('#overlay').addClass('close');
         }, 1000);
     });
+
+    $('#overlay').on('click', 'a[href^="#"]', function (e) {
+        $('#overlay').addClass('animated fadeOut');
+        $("section").css("display", "block");
+        $("#Footer").css("display", "grid");
+        setTimeout(function() {
+            $('#overlay').addClass('close');
+        }, 1000);
+    });
+});
+
+$(document).on('click', 'a[href^="#"]', function (e) {
+    e.preventDefault();
+    $('html, body').stop().animate({
+        scrollTop: $($(this).attr('href')).offset().top
+    }, 600);
+
 });
